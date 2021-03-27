2b63db1fbdf7076f6af7dd60b0095a6cadcc250b
annaarakelyan98 <50499890+annaarakelyan98@users.noreply.github.com>
2021-03-27 16:43:03 +0400

    Delete design-patterns directory

--- design-patterns/src/main/java/com/designPatterns/creational/factory/abstractClass/London.java
+++
@@ -1,8 +0,0 @@
-package com.designPatterns.creational.factory.abstractClass;
-
-public class London extends CityName {
-    @Override
-    void getName() {
-        System.out.println("Capital city of Great Britain!");
-    }
-}
--- design-patterns/src/main/java/com/designPatterns/creational/factory/byInterface/shapeCreate/Circle.java
+++
@@ -1,8 +0,0 @@
-package com.designPatterns.creational.factory.byInterface.shapeCreate;
-
-public class Circle implements Shape {
-    @Override
-    public void draw() {
-        System.out.println("Inside Circle::draw() method!");
-    }
-}
